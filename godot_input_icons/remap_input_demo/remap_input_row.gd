7ac81d4571390286cc07403ff1714b68beb58e7f
RcubDev <rlinehan2@gmail.com>
2026-06-27 18:58:04 -0700

    Restructure demo for controller navigation

--- godot_input_icons/remap_input_demo/remap_input_row.gd
+++ godot_input_icons/remap_input_demo/remap_input_row.gd
@@ -3,11 +3,13 @@ class_name RemapInputRow
 @export var action_name: String = ""
 @export var input_icon_texture_rect: InputIconTextureRect
 var row_label: Label
+var row_button: Button
 signal remap_action(action: String, icon_texutre: InputIconTextureRect)
 
 func _ready() -> void:
 	input_icon_texture_rect = get_node("%InputIconTextureRect")
 	row_label = get_node("%RowLabel")
-	(get_node("%RemapButton") as Button).pressed.connect(
+	row_button = get_node("%RemapButton")
+	row_button.pressed.connect(
 			func(): emit_signal("remap_action", action_name, input_icon_texture_rect)
 		)
